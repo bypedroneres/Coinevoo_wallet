33184fa72ec253b5d4a2772b86e2bf90f52460d6
Pedro Neres <bypedroneres@gmail.com>
2023-10-03 22:06:50 -0300

    commented the css properly

--- javascript/wallet.js
+++ javascript/wallet.js
@@ -1,17 +1,13 @@
-// Replace with your actual BitGo API URL and API token
 const bitgoApiUrl = "https://www.bitgo.com/api/v2/";
 const bitgoApiToken = "your-api-token-here";
 
-// Replace with your actual wallet ID or address
 const walletId = "your-wallet-id-or-address-here";
 
-// Function to update the balance in HTML
 function updateBalance(balance) {
     const balanceElement = document.getElementById("balance-amount");
     balanceElement.textContent = `$${balance.toFixed(2)}`;
 }
 
-// Fetch the wallet balance from BitGo API
 fetch(`${bitgoApiUrl}wallet/${walletId}/balance`, {
     headers: {
         "Authorization": `Bearer ${bitgoApiToken}`,
@@ -19,8 +15,7 @@ fetch(`${bitgoApiUrl}wallet/${walletId}/balance`, {
 })
 .then(response => response.json())
 .then(data => {
-    // Extract the balance from the API response
-    const balance = data.balance / 100000000; // Convert Satoshis to BTC
+    const balance = data.balance / 100000000; 
     updateBalance(balance);
 })
 .catch(error => {
